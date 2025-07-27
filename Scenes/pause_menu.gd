39055b31a336e424b4cc6b0652f5b353ef54af53
anxapa <31834487+anxapa@users.noreply.github.com>
2025-07-27 00:56:30 -0700

    Added pause menu
    - Added pause menu with some buttons
    - Functional, with the exception of other buttons that leads to other scenes other than the game

---
+++ Scenes/pause_menu.gd
@@ -0,0 +1,52 @@
+extends Panel
+
+@export var animation_player : AnimationPlayer
+
+# Buttons (mainly for disabling / enabling them)
+@onready var home_button := $"Pause Panel/Home Button"
+@onready var settings_button := $"Pause Panel/Settings Button"
+@onready var upgrades_button := $"Pause Panel/Upgrades Button"
+@onready var back_button := $"Pause Panel/Back Button"
+
+func _ready() -> void:
+	# CRITICAL: Temporarily disabling the buttons not functionable yet
+	disable_button(home_button)
+	disable_button(settings_button)
+	disable_button(upgrades_button)
+
+func _process(_delta: float) -> void:
+	# Checks for whenever the player is using an escape key
+	if Input.is_action_just_pressed("ui_cancel"):
+		# Different behaviors depending on the game state
+		# Uh should be pretty self-explanatory
+		match GameManager.current_game_state:
+			GameManager.GameStates.PAUSED:
+				unpause()
+			GameManager.GameStates.GAME:
+				pause()
+
+func pause() -> void:
+	GameManager.current_game_state = GameManager.GameStates.PAUSED
+	animation_player.play("on_pause")
+	get_tree().paused = true
+
+func unpause() -> void:
+	GameManager.current_game_state = GameManager.GameStates.GAME
+	animation_player.play("unpause")
+	get_tree().paused = false
+
+func enable_button(button: Button) -> void:
+	button.disabled = false
+	button.modulate = Color.WHITE
+
+func disable_button(button: BaseButton) -> void:
+	button.disabled = true
+	button.modulate = Color.GRAY
+
+# Functionality when back button is pressed (duh)
+func _on_back_button_pressed() -> void:
+	unpause()
+
+# Functionality when background is pressed (duh)
+func _on_background_button_pressed() -> void:
+	unpause()
--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -8,3 +8,13 @@ var audio_manager: Node2D
 
 # Game stats
 var cash_collected : float
+
+# Game states
+enum GameStates {
+	MAIN_MENU,
+	GAME,
+	PAUSED,
+	SHOP,
+}
+
+var current_game_state := GameStates.GAME
