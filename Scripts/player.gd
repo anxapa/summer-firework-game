1ce3b7fee938ea8a763c51be6360d4156a18a8fd
Robin <148491884+a-robin23@users.noreply.github.com>
2025-07-28 00:30:06 -0700

    added panning to rocket SFX

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -77,9 +77,12 @@ func movement(delta: float) -> void:
 	# Put velocity y into scroll speed
 	scroll_speed = velocity.y
 	velocity.y = 0
+
 	if GameManager.current_game_state == GameManager.GameStates.GAME:
+		$ThrustSound.set_parameter("Panning", (global_position.x - 960)/19.2)
 		$ThrustSound.play(false)
-		
+		print_debug((global_position.x - 960)/19.2)
+	
 	move_and_slide()
 
 func apply_gravity(delta: float) -> void:
