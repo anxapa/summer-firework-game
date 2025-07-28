c24581f8f5185fa7bbc6793c7564444c2724704c
Robin <148491884+a-robin23@users.noreply.github.com>
2025-07-27 18:54:23 -0700

    added spawn_manager logic

--- Cash Patterns/patterns.gd
+++ Cash Patterns/patterns.gd
@@ -1,28 +1,36 @@
 # TODO: Remove critical sections prior to game launch
+# 		Add Weight support to animations
+
+# NOTE: AnimationPlayer patterns should be no bigger than 128x128 wide (centered on the node anchors) to accomodate spawn_manager and cash sprite size.
 
 class_name Patterns
 extends Node2D
 @export var cash_value := 1
-var starting_position
+var spawn_position
 # false indicates fixed spawn
 var spawned = false
 @onready var animation_player := $AnimationPlayer
 var pattern_ : String
 
+# Static Variables
 static var scene := load("res://Cash Patterns/patterns.tscn")
+static var patterns := ["diamond", "smile", "line", "plus"]
 
 
 func _ready() -> void:
-	animation_player.play(pattern_)
-	global_position = starting_position
-
+	animation_player.current_animation = pattern_
+	global_position = spawn_position
+	
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		GameManager.cash_collected += cash_value
 		queue_free()
 
-static func spawn(starting_position: Vector2, pattern: String) -> Node2D:
+static func spawn(spawn_position: Vector2, pattern: String) -> Node2D:
 	var spawn : Node2D = scene.instantiate()
 	spawn.pattern_ = pattern
-	spawn.starting_position = starting_position - Vector2(0, starting_position.y)
+	spawn.spawn_position = spawn_position
 	return spawn
+	
+static func get_rand_pattern() -> String:
+	return patterns.pick_random()
--- Scripts/spawn_manager.gd
+++ Scripts/spawn_manager.gd
@@ -1,8 +1,44 @@
+class_name SpawnManager
 extends Node2D
 @export var cash_parent : Node2D
 @export var cloud_parent : Node2D
+@export var timer : Timer
+@onready var player_manager := GameManager.player_manager
+
+# Strange OFF_SCREEN constant to prevent animationPlayer flickering 
+const OFF_SCREEN_Y = 1080/2 + 312
+
+# Determines timing between pattern spawns (this should change according to player speed in some way)
+var p_interval_min := 2
+var p_interval_max := 3
+
+# Determines many seconds worth of travel is needed to reach the next pattern
+var p_x_min := 0.0
+var p_x_max := 0.0
+
+var p_y_min := 2
+var p_y_max := 3
+
 
 func _ready() -> void:
-	var test = Patterns.spawn(GameManager.player_manager.player.global_position, "diamond")
+	GameManager.player_manager.player.initial_velocity
+	var x = randf_range(p_interval_min, p_interval_max)
+	timer.wait_time = x
+	timer.timeout.connect(_spawn)
+
+# Spawning Guidelines?
+
+# Patterns should be spawned at least one seconds worth of travel above the player
+# Patterns should never be spawned more than two seconds worth of y travel above the player--unless two seconds worth of travel is on-screen
+# Patterns will be spawned every x seconds determined by the interval
+#
+# ... I think all of these need to be scrapped
+func _physics_process(delta: float) -> void:
+	pass
+
+func _spawn() -> void:
+	var p_y_distance := randf_range(p_y_min * player_manager.get_scroll_speed(), p_y_max * player_manager.get_scroll_speed())
+	var p_x_distance := 0
+	
+	var test = Patterns.spawn(player_manager.player.global_position + Vector2(p_x_distance, min(p_y_distance, -OFF_SCREEN_Y)), Patterns.get_rand_pattern())
 	cash_parent.add_child(test)
-	print_debug("help")
